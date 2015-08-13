87207d87e6091123aedf935c5b2a1f5780f03fbd
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-08-13 12:51:35 +0200

    WebSharper.UI.Next SPA template

--- templates/websharperspa/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Inherits="<%= namespace %>.Global" %>
--- templates/websharperspa/Global.asax.fs
+++
@@ -1,7 +0,0 @@
-namespace <%= namespace %>
-
-type Global() =
-    inherit System.Web.HttpApplication()
-
-    member g.Application_Start(sender: obj, args: System.EventArgs) =
-        ()
